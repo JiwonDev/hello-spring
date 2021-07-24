473597c4920b3c387220b438de3b1b8c82f69e5a
JiwonDev <sylvester7412@gmail.com>
2021-07-24 18:27:52 +0900

    스프링부트 예제1 간단한 회원 조회 만들기

--- src/main/java/hello/hellospring/HelloSpringApplication.java
+++ src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -1,13 +1,15 @@
 package hello.hellospring;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class HelloSpringApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HelloSpringApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSpringApplication.class, args);
+    }
 
 }
